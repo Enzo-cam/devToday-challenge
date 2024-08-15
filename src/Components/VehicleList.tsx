ea28c5287c98d0675ec8fc6e1e190e34589e81bd
enzo-cam <enzo.fcam@gmail.com>
2024-08-15 01:27:32 -0300

    Feat: better UI of the models

--- src/Components/VehicleList.tsx
+++ src/Components/VehicleList.tsx
@@ -6,17 +6,27 @@ type Vehicle = {
     Model_Name: string;
 }
 
+// VehicleList component
+// This component will display a list of vehicles passed as props
 const VehicleList = ({vehicles} : {vehicles: Vehicle[]}) => {
     console.log(vehicles)
-  return (
+  
+    if(!vehicles || vehicles.length === 0) {
+        return <div className="text-center text-lg">No vehicles found with the selected criteria</div>
+    }
+  
+    return (
     
-    <ul className="space-y-2">
+    <ul className="space-y-4">
         { vehicles.map((vehicle) => (
-            <li key={vehicle.Model_ID} className="bg-white p-4 rounded shadow">
-                <div className="flex justify-between">
+            <li key={vehicle.Model_ID} className="bg-white p-4 rounded shadow-md hover:shadow-lg transition-shadow duration-300">
+                <div className="flex justify-between items-center">
                     <div>
-                        <h2 className="text-xl font-bold">{vehicle.Make_Name}</h2>
-                        <p className="text-lg">{vehicle.Model_Name}</p>
+                        <h2 className="text-2xl font-bold text-blue-950">{vehicle.Make_Name}</h2>
+                        <p className="text-lg text-gray-700 mt-1">{vehicle.Model_Name}</p>
+                    </div>
+                    <div className="bg-gray-100 px-3 py-1 rounded-md">
+                        <p className=" text-gray-700">Model ID: {vehicle.Model_ID}</p>
                     </div>
                 </div>
             </ li>
