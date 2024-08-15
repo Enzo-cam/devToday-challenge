8b2cf4e5b536c26d1863e34302cffedf3e9287d8
enzo-cam <enzo.fcam@gmail.com>
2024-08-15 01:56:51 -0300

    Prettier and linter

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "react"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "prettier",
+  ],
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  rules: {
+    
+    "react/jsx-uses-react": "off",
+    "react/react-in-jsx-scope": "off"
+  }
+};
